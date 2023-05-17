40d03d4283b36fd3aef88e5d191a62f79a3174b0
Emilse Arias <emilsearias@gmail.com>
2023-05-17 10:39:53 -0300

    agregamos el maquetado de app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from "react-bootstrap";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  
-
   return (
     <>
-    <h1>Titulo de prueba</h1>
+      <Container className="my-5 mainPage">
+        <h1 className="display-4 text-center text-light">Lista de tareas</h1>
+        <hr className="text-light" />
+        {/* aqui agrego el formulario */}
+      </Container>
+      <footer className="bg-dark text-light text-center py-4">
+        <p>&copy; Todos los derechos reservados</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
