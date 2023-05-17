81d4ff0628527cec93ba953bca59a55b4aa450db
Emilse Arias <emilsearias@gmail.com>
2023-05-17 11:01:51 -0300

    agregamos los componentes ItemTarea y ListaTareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Container } from "react-bootstrap";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Formulario from "./components/Formulario";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <h1 className="display-4 text-center text-light">Lista de tareas</h1>
         <hr className="text-light" />
         {/* aqui agrego el formulario */}
+        <Formulario></Formulario>
       </Container>
       <footer className="bg-dark text-light text-center py-4">
         <p>&copy; Todos los derechos reservados</p>
